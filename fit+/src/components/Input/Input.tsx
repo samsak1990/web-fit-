7473321d9ec633bc0ffacf1a0bce871b8ffc0202
shues <shues@bk.ru>
2025-06-24 11:32:16 +0300

    inputs

--- fit+/src/components/Input/Input.tsx
+++ fit+/src/components/Input/Input.tsx
@@ -3,10 +3,51 @@ import styles from './Input.module.css';
 
 type TInputProps = {
   placeholder: string;
+  value: string;
+  setValue(value: string): void;
+  error?: string;
+  secret?: boolean;
 };
 
-const Input: React.FC<TInputProps> = ({ placeholder }) => {
-  return <input className={styles.input} placeholder={placeholder} />;
+const Input: React.FC<TInputProps> = ({ placeholder, value, setValue, error = "", secret = false }) => {
+
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  function updateOpen(): void {
+    if (error === "" && value !== "" && secret) {
+      setOpen(prev => !prev);
+    }
+  }
+
+  let type = "text";
+
+  if (secret && !open) {
+    type = "password";
+  }
+
+  return (
+    <div className={styles.input__wrapper}>
+      <input
+        className={styles.input + (error === "" ? "" : " " + styles.input_error)}
+        placeholder={placeholder}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        type={type}
+      />
+      <div className={
+        styles.input__info +
+        ((error !== "" && value !== "") ? " " + styles.input__info_error : "") +
+        ((error !== "" && value === "") ? " " + styles.input__info_warning : "") +
+        ((error === "" && value !== "" && secret && open) ? " " + styles.input__info_open : "") +
+        ((error === "" && value !== "" && secret && !open) ? " " + styles.input__info_closed : "")
+      }
+        onClick={updateOpen}
+      ></div>
+      <div className={styles.error + (error === "" ? "" : " " + styles.error_show)}>
+        {error}
+      </div>
+    </div>
+  );
 };
 
 export default Input;
